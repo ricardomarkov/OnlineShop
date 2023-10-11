79548a64442c3251d8ed1292abdaa76d1ec2bcf0
Ricardo Markov <ricardomarkov@gmail.com>
2023-10-11 16:39:08 -0600

    Navbar

---
+++ src/Components/navbar/index.jsx
@@ -0,0 +1,98 @@
+import { NavLink } from "react-router-dom"
+import './navbar.css'
+const Navbar= () =>{
+const activeStyle= 'underline underline-offset-4'
+    return (
+        <nav className='flex justify-between items-center  fixed z-10 w-full py-5 px-8 text-sm font-light'>
+            <ul className='flex items-center gap-3'>
+                <li className='font-semibold text-lg'>
+                    <NavLink to='/'>
+                        OnlineShop
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : ""
+                    }>
+                        All
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/clothes'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : ""
+                    }>
+                        Clothes
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/electronics'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : ""
+                    }>
+                        Electronics
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/furnitures'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : ""
+                    }>
+                        Furnitures
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/toys'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : ""
+                    }>
+                        Toys
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/others'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : ""
+                    }>
+                        Others
+                    </NavLink>
+                </li>
+            </ul>
+            <ul className='flex items-center gap-3'>
+                <li className='text-black/60'>
+                    user@example.com
+                </li>
+                <li>
+                    <NavLink to='/my-orders'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : ""
+                    }>
+                        My Orders
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/my-account'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : ""
+                    }>
+                        My Account
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/sign-in'
+                    className={({ isActive }) =>
+                    isActive ? activeStyle : ""
+                    }>
+                        Sign-in
+                    </NavLink>
+                </li>
+                <li>
+                🛒 0
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,21 +1,32 @@
-import './App.css'
+import {useRoutes, BrowserRouter} from 'react-router-dom'
 import Home from "../Home"
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+import Navbar from '../../Components/navbar'
 
-function App() {
+import './App.css'
+
+const AppRoutes = () =>{
+  let routes = useRoutes([
+    {path: '/',element: <Home/>},
+    {path: '/my-account',element: <MyAccount/>},
+    {path: '/my-order',element: <MyOrder/>},
+    {path: '/my-orders',element: <MyOrders/>},
+    {path: '/*',element: <NotFound/>},
+    {path: '/sign-in',element: <SignIn/>},
+  ])
+  return routes;
+}
+
+const App = ()=> {
   return (
-      <div className="bg-red-100">
-        <Home/>
-        <MyAccount />
-        <MyOrder />
-        <MyOrders />
-        <NotFound />
-        <SignIn />
-      </div>
+      <BrowserRouter>
+      <AppRoutes/>
+      <Navbar/>
+      </BrowserRouter>
   )
 }
 
