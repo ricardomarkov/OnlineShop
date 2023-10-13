04a07166e886b32c6ee9de6dfb98fb0219c0a0f4
Ricardo Markov <ricardomarkov@gmail.com>
2023-10-12 19:59:24 -0600

    Testing Fake API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,20 +4,26 @@ import Card from '../../Components/Card'
 
 function Home() {
 //setItems toma la informacion y la guarda en items.
-  const [items, setItems] = useState(null)
+  const [data, setItems] = useState(null)
 
   useEffect(()=>{
-// fetch('https://dummyjson.com/products')
-fetch('https://api.escuelajs.co/api/v1/products')
-.then( response => response.json())
-.then( data => setItems(data))
+//fetch('https://dummyjson.com/products')
+//fetch('https://api.escuelajs.co/api/v1/products')
+// fetch('https://fakestoreapi.com/products/1')
+//.then( response => response.json())
+// .then( data => setItems(data))
+//   },[])
+
+fetch('https://fakestoreapi.com/products/1')
+.then(res=>res.json())
+.then(data=>console.log(data))
   },[])
 
   return (
       <Layout>
         Home
         {
-          items?.map(item => (
+          data?.map(item => (
             <Card key={item.id} data={item} />
           ))
         }
