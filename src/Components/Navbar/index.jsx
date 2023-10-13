97fd954cd5ca0430d5dd3cab5da233979c66f841
Ricardo Markov <ricardomarkov@gmail.com>
2023-10-12 22:12:57 -0600

    ShippingCart counter added

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
 import { NavLink } from 'react-router-dom'
+import { ShoppingCartContext } from '../../Context'
 
 const Navbar= () =>{
-const activeStyle= 'underline underline-offset-4'
+    const context = useContext(ShoppingCartContext)
+    const activeStyle= 'underline underline-offset-4'
     return (
         <nav className='flex justify-between items-center  fixed z-10 w-full py-5 px-8 top-0 text-sm font-light'>
             <ul className='flex items-center gap-3'>
@@ -88,7 +91,7 @@ const activeStyle= 'underline underline-offset-4'
                     </NavLink>
                 </li>
                 <li>
-                🛒 0
+                🛒 {context.count}
                 </li>
             </ul>
         </nav>
---
+++ src/Context/index.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from 'react';
+
+export const ShoppingCartContext = createContext();
+
+export const ShoppingCartProvider = ({children}) => {
+    const [count, setCount] = useState(0)
+    console.log('COUNT: ', count)
+    return(
+        <ShoppingCartContext.Provider value={{
+            count,
+            setCount
+        }}>
+        {children}
+        </ShoppingCartContext.Provider>
+    )
+}
